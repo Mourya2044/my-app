f8d14612358135e44afdd6efdb2d46b0174f0caa
Mourya <mourya.saha.2004@gmail.com>
2024-12-29 14:56:48 +0530

    tutorial completed for textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        // console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert('Converted to Uppercase', 'success');
     }
 
     const handleLoClick = () => {
-        // console.log("Uppercase was clicked");
         let newText = text.toLowerCase();
         setText(newText);
         props.showAlert('Converted to Lowercase', 'success');
@@ -21,7 +19,6 @@ export default function TextForm(props) {
     }
 
     const handleOnChange = (e) => {
-        // console.log(e);
         setText(e.target.value);
     }
 
@@ -39,19 +36,19 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={ text } placeholder='Enter text here' onChange={ handleOnChange } id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={ handleUpClick }>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={ handleLoClick }>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={ handleCopy }>Copy</button>
-                <button className="btn btn-danger mx-1" onClick={ handleClear }>Clear Text</button>
+                <button disabled={text.length===0               } className="btn btn-primary mx-1 my-1" onClick={ handleUpClick }>Convert to UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={ handleLoClick }>Convert to LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={ handleCopy }>Copy</button>
+                <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={ handleClear }>Clear Text</button>
             </div>
             <div className="container my-3">
                 <h1>Your Text Summary</h1>
-                <p>{ text.length > 0 ? text.split(' ').length : 0} words and { text.length } characters</p>
-                <p>{0.008 * text.split(' ').length} minutes read</p>
+                <p>{ text.split(/\s+/).filter((element)=>{ return element.length!==0 }).length} words and { text.length } characters</p>
+                <p>{ 0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} minutes read</p>
             </div>
             <div className="container my-3">
                 <h1>Preview</h1>
-                <p>{ text.length > 0 ? text : "Enter something in the textbox above to preview it here" }</p>
+                <p>{ text.length > 0 ? text : "Nothing to preview" }</p>
             </div>
         </>
     )
